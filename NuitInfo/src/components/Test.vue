7e288d1202ee122226fe901883d143cd41d1af93
etienneamiche <etienne.amiche@gmail.com>
2018-12-06 21:58:07 +0100

    Front ajout Vue project

---
+++ NuitInfo/src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-card>
+      <h1>{{ msg }}</h1>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Dashboard'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h1, h2 {
+  color: tomato;
+}
+
+</style>
