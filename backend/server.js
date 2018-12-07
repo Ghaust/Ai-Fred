0dcd7a0045f1c04653d75dbadab25e96c9aea2e5
carage34 <jully.morgan@orange.fr>
2018-12-07 06:17:01 +0000

    moi

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ var express = require('express')
       
       })
 
+      
+      app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+        next()
+      })
 
 setInterval(function (){
  
@@ -26,22 +32,24 @@ setInterval(function (){
   con.query(req, function(err, res){
     if(err) throw err
     res.forEach(function(data) {
-      randheart = globalFunc.getRandomInt(-20, 20);
-      randtemp = globalFunc.getRandomInt(-5, 5);
-      if((data.heartbeat >= 50+(randheart)) || (data.heartbeat+(randheart) <= 200 )){
+      randheart = globalFunc.getRandomInt(-3, 3);
+      randtemp = globalFunc.getRandomInt(-2, 2);
+
+      if((data.heartbeat+(randheart) >= 50) || (data.heartbeat+(randheart) <= 200 )){
         heartbeat = data.heartbeat + randheart
       } else {
         heartbeat = data.heartbeat
       }
 
-      if((data.body_temperature >= 37+randtemp) || (data.body_temperature <= 50+randtemp)) {
+      if((data.body_temperature+(randtemp) >= 37) || (data.body_temperature+(randtemp) <= 50)) {
            body_temperature = data.body_temperature + randtemp
       } else {
         body_temperature = data.body_temperature
       }
+      //heartbeat = 100
+      //body_temperature = 40
       console.log("HTTP PUT Request : Add ")
-      sql_req = "INSERT INTO Explorer (heartbeat, weight, body_temperature, latitude, longitude) VALUES ('" + heartbeat + "','" 
-      +  data.weight + "','" + body_temperature +  "','" + data.longitude + "','"
+      sql_req = "INSERT INTO Explorer (heartbeat, weight, body_temperature, latitude, longitude) VALUES ('" + heartbeat + "','" +  data.weight + "','" + body_temperature +  "','" + data.longitude + "','"
       + data.latitude + "');" 
       this.con.query(sql_req, function(err, res) {
       if (err) throw err
@@ -51,6 +59,38 @@ setInterval(function (){
 
 })
 },1000);
+
+setInterval(function (){
+
+    
+  req = "SELECT * from Material";
+    con.query(req, function(err, res){
+      if(err) throw err
+      
+      console.log(res);
+      
+      res.forEach(function(data) {
+        console.log(data.id)
+        console.log(data.status)
+        console.log("HTTP PUT Request : Add ")
+        status="OK"
+        if(data.status=="OK" ){
+          status="NOK"
+        } else {
+          status="OK"
+        }
+        con.query("UPDATE Material SET status ='" + status +  "' WHERE id = " + data.id , function(err, res) {
+        if (err) throw err
+        console.log(err);  
+        console.log("New element updated.")
+        })
+      });
+  
+  })
+
+  
+  
+},100000);
 
 
 
@@ -111,4 +151,38 @@ app.get('/Equipment/getTechnicalInfos', function (req, resultat){
    })
          
 
-       
+app.get('/Material/alertBattery', function(req, resultat){
+  console.log("HTTP GET : Get Alert Battery")
+  sql_req = "SELECT name, battery from Material where battery<40;"
+  con.query(sql_req, function(err, res){
+    if(err) throw err
+    resultat.send(res)
+  })
+})
+
+app.get('/Material/statusBattery', function(err, resultat){
+  console.log("HTTP GET : Get Status Battery All Devices")
+  sql_req = "SELECT name, battery from Material"
+  con.query(sql_req, function(err, res){
+    if(err) throw err
+    resultat.send(res)
+  })
+})
+
+app.get('/Stock/alertInsufficientStock', function(err, resultat){
+  console.log("HTTP GET : Get Insufficient Stock")
+  sql_req = "SELECT name, quantity from Stock where quantity<20"
+  con.query(sql_req, function(err, res){
+      if(err) throw err
+      resultat.send(res)
+  })
+})
+
+app.get('/Equipment/DamagedEquipment', function(err, resultat){
+  console.log("HTTP GET : Get Equipment Status")
+  sql_req = "SELECT name, taken, status from Equipment where status='ko'"
+  con.query(sql_req, function(err, res){
+    if(err) throw err
+    resultat.send(res)
+  })
+})
